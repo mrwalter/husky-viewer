64ec5d7b58221c2173a8b13f20f30e1ade5bb23f
husky-ripl <husky@ttic.edu>
2018-09-19 15:57:18 -0500

    added submodules

---
+++ path-utils/README.md
@@ -0,0 +1,10 @@
+# Path Utils
+
+A library for querying various project-specific paths
+
+The code follows the pods build framework (https://sourceforge.net/p/pods/home/Home/).
+
+## Requirements
+
+* CMake
+* Python
---
+++ path-utils/src/path_util.h
@@ -0,0 +1,22 @@
+#ifndef __path_util__
+#define __path_util__
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+const char * getBasePath();
+const char * getDataPath();
+const char * getModelsPath();
+const char * getConfigPath();
+const char * getMapsPath();
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
